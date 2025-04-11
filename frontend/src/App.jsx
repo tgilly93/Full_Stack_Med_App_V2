a05060d3efe34c7c7652c781437f2d46c7203129
Terry Gilmore <tgilly93@gmail.com>
2025-04-11 09:33:18 -0400

    TG install bts/clean default code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";  
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container fluid className="vh-100 d-flex justify-content-center align-items-center bg-light">
+      <Row>
+        <Col className="text-center">
+          <h1>This is the landing page!</h1>
+          <p>Welcome to my fullstack application!</p>
+          </Col>
+      </Row>
+    </Container>
   )
 }
 
