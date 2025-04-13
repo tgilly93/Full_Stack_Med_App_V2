749e1961a231defcbc1dbb483c9383fa73df84a4
Terry Gilmore <tgilly93@gmail.com>
2025-04-13 09:46:58 -0400

    TG added Landing page & set up routing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";  
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+ 
 
 
 
 function App() {
   
-
   return (
-    <Container fluid className="vh-100 d-flex justify-content-center align-items-center bg-light">
-      <Row>
-        <Col className="text-center">
-          <h1>This is the landing page!</h1>
-          <p>Welcome to my fullstack application!</p>
-          </Col>
-      </Row>
-    </Container>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+    </Routes>
   )
 }
 
