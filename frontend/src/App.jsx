9613e86445951f0dbbadadbc8f12f0586a9f82bf
Terry Gilmore <tgilly93@gmail.com>
2025-04-15 09:19:47 -0400

    TG added NavBar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
+import NavBar from "./components/NavBar";
  
 
 
 
 function App() {
-  
   return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-    </Routes>
-  )
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App
