7d39294ac5a279fad8b894df0c3b0fb5ff273dca
Terry Gilmore <tgilly93@gmail.com>
2025-04-27 10:10:25 -0400

    TG added dashboard & appt comp.

---
+++ frontend/src/components/MyAppointments.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyAppointments() {
+  return (
+    <div className="bg-white p-3 border rounded shadow-sm">
+      <h3>My Appointments</h3>
+      <p>List of appointments to go here....</p>
+    </div>
+  );
+}
+
+export default MyAppointments;
