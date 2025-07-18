6a38136cbd45158bd2cd051de20c45212b445abd
Terry Gilmore <tgilly93@gmail.com>
2025-07-18 09:15:38 -0400

    TG catch up.

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import NavBar from "../components/Navbar";
+import EditProfile from "../components/EditProfile";
+import Appointment from "../components/Appointment";
+import MyAppointments from "../components/MyAppointments";
+
+function Dashboard() {
+  return (
+    <>
+      <NavBar />
+
+    <Container
+      fluid
+      className="min-vh-100 bg-light p-4"
+    >
+      <h1 className="text-center mb-4">Welcome to Your Dashboard!</h1>
+      <Row>
+        <Col md={6} className="mb-4">
+          <EditProfile />
+        </Col>
+        <Col md={6} className="mb-4">
+          <Appointment />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <MyAppointments />
+        </Col>
+      </Row>
+    </Container>
+    </>
+  );
+}
+
+export default Dashboard;
