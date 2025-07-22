611d2c99f2bb7229a15f47ef4dfc3131bc9b1888
Terry Gilmore <tgilly93@gmail.com>
2025-07-22 11:47:49 -0400

    TG import pages to app routing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Footer from "./components/Footer";
 import AdminDashboard from "./pages/AdminDashboard";
+import ClinDashboard from "./pages/ClinDashboard";
+import StaffDashboard from "./pages/StaffDashboard";
+import PatientDashboard from "./pages/PatientDashboard";
+import ReceptionistDashboard from "./pages/ReceptionistDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
  
 
 function App() {
