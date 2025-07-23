a05cd031bfee160533fb3a8680fcbdac9e78744d
Terry Gilmore <tgilly93@gmail.com>
2025-07-23 09:02:00 -0400

    TG added logic for routing by role.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,61 @@ import StaffDashboard from "./pages/StaffDashboard";
 import PatientDashboard from "./pages/PatientDashboard";
 import ReceptionistDashboard from "./pages/ReceptionistDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
- 
 
 function App() {
   return (
     <div className="App d-flex flex-column min-vh-100">
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
+
+        <Route
+          path="/dashboard-admin"
+          element={
+            <ProtectedRoute allowedRoles={["ROLE_ADMIN"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard-clin"
+          element={
+            <ProtectedRoute allowedRoles={["ROLE_CLINICIAN"]}>
+              <ClinDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard-staff"
+          element={
+            <ProtectedRoute allowedRoles={["ROLE_STAFF"]}>
+              <StaffDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard-patient"
+          element={
+            <ProtectedRoute allowedRoles={["ROLE_PATIENT"]}>
+              <PatientDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard-receptionist"
+          element={
+            <ProtectedRoute allowedRoles={["ROLE_RECEPTIONIST"]}>
+              <ReceptionistDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
       <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
