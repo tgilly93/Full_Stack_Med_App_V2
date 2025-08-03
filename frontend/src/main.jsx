163ff8cef13bf1369daebf2334aa4bc41238389e
Terry Gilmore <tgilly93@gmail.com>
2025-08-03 08:29:11 -0400

    TG added LogoutButton to Nav.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
-import store from './store.js';
+import store from './store/store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
